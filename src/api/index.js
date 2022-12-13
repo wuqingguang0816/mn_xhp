f6ce44790570d3b20328dae400fc5ddb04c8f46f
chen hong <18674876733@163.com>
2022-12-13 10:21:51 +0800

    提交代码 by Alan

--- src/api/index.js
+++ src/api/index.js
@@ -4,7 +4,7 @@ let u = "/LIMS"
     // 测试接口
 export const getReq = (url, method, data) => {
     return requests({
-        url: u + url,
+        url: url,
         method: method,
         data: data
     })
